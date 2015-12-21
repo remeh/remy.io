c7206c7b3ad91cc9a6cf93ad7f3b7ac7ff9e94f6
remeh <remeh@remeh.fr>
2015-12-21 21:02:14 +0100

    Splashscreen before video.

---
+++ web/mehstation.js
@@ -0,0 +1,33 @@
+var player;
+var player_target;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '290',
+    width: '100%',
+    videoId: 'FxudzfhMTlU',
+    events: {
+      'onReady': onPlayerReady,
+      //'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  player_target = event.target;
+}
+
+function onStart() {
+  var o = document.querySelectorAll('#overplayer');
+  var p = document.querySelectorAll('#player');
+  console.log(o);
+  console.log(p);
+  if (o.length > 0 && p.length > 0 && player_target) {
+    o[0].style.visibility = 'hidden';
+    o[0].style.display = 'none';
+    p[0].style.visibility = 'visible';
+    p[0].style.display = 'inherit';
+    p[0].style.height = '620px';
+    player_target.playVideo();
+  }
+}
