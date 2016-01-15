8c617b608b0f3bf30a7ae07812498419469f8bef
remeh <remeh@remeh.fr>
2016-01-15 15:46:55 +0100

    Clean up runtime code.

--- main.go
+++ main.go
@@ -4,24 +4,15 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
-type CvHandler struct {
-}
-
-func (c *CvHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Location", "/cv.html")
-	w.WriteHeader(302)
-}
-
 func main() {
-	// CV redirects of old uris.
-	//cv := CvHandler{}
-	//	http.Handle("/cv", &cv)
-	//http.Handle("/resume", &cv)
-
-	// And static serving.
-	http.Handle("/", http.FileServer(http.Dir("web/")))
+	if len(os.Getenv("DOCKER")) > 0 {
+		http.Handle("/", http.FileServer(http.Dir("/remy.io/web/")))
+	} else {
+		http.Handle("/", http.FileServer(http.Dir("web/")))
+	}
 
 	http.ListenAndServe(":8080", nil)
 }
